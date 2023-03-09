5e9762ee5981b19d8946096fbd8d136a756978ce
bytengineer <kevin.missback@googlemail.com>
2023-03-09 22:05:00 +0100

    create some example data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Guest;
+use App\Models\Invitation;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +21,22 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Invitation::create([
+            'recipient' => 'The Doe Family',
+            'password' => 'password'
+        ]);
+
+        Guest::create([
+            'firstname' => 'John',
+            'lastname' => 'Doe',
+            'invitation_id' => 1
+        ]);
+
+        Guest::create([
+            'firstname' => 'Jane',
+            'lastname' => 'Doe',
+            'invitation_id' => 1
+        ]);
     }
 }
