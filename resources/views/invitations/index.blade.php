d849da6d2db12a81a8702c9684253d80ff2a6929
bytengineer <kevin.missback@googlemail.com>
2023-03-13 14:48:18 +0100

    better table layout

--- resources/views/invitations/index.blade.php
+++ resources/views/invitations/index.blade.php
@@ -5,25 +5,41 @@
 
 @unless(count($invitations) == 0)
 
-<table>
-    <thead>
-        <tr>
-            <td>ID</td>
-            <td>Recipient</td>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($invitations as $invitation)
-        <tr>
-            <td>{{ $invitation->id }}</td>
-            <td>{{ $invitation->recipient }}</td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+@foreach($invitations as $invitation)
+<div class="invitation">
+    <h2>{{ $invitation->recipient }}</h2>
+    <div class="invitation-guests">
+        @unless(count($invitation->guests) == 0)
+        <table>
+            <thead>
+                <tr>
+                    <td>ID</td>
+                    <td>Firstname</td>
+                    <td>Lastname</td>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($invitation->guests as $guest)
+                <tr>
+                    <td>{{ $guest->id }}</td>
+                    <td>{{ $guest->firstname }}</td>
+                    <td>{{ $guest->lastname }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        @endunless
+        <div class="guest-create">
+            Add new Guest
+        </div>
+    </div>
+</div>
+@endforeach
 
 @else
 <p>No invitations here</p>
 @endunless
+
+{{ $invitations->links() }}
 
 @endsection
