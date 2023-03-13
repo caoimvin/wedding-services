102e5a51d8876dcd17b1c5ebce327fd46584527b
bytengineer <kevin.missback@googlemail.com>
2023-03-13 14:49:38 +0100

    use foreignId type and cascade delete

--- database/migrations/2023_03_08_213359_create_guests_table.php
+++ database/migrations/2023_03_08_213359_create_guests_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('guests', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('invitation_id')->constrained()->onDelete('cascade');
             $table->string('firstname');
             $table->string('lastname');
-            $table->integer('invitation_id');
             $table->timestamps();
         });
     }
